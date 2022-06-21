76b113956bd20d5b07e97fb78bbe073c4dbc26ec
ysrckr <dyt.yasarcakir@gmail.com>
2022-06-22 01:09:15 +0300

    jsconfig modified

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import Showcase from '@/components/Showcase'
-import styles from '@/styles/Layout.module.css'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Showcase from '@components/Showcase'
+import styles from '@styles/Layout.module.css'
 export default function Layout({ title, keywords, descrripton, children }) {
 	const router = useRouter()
 	return (
