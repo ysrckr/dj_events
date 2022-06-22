cdef6c1e04fa778d6e0610068236cef223d0d8f0
ysrckr <dyt.yasarcakir@gmail.com>
2022-06-22 21:10:51 +0300

    bfr strapi

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,51 @@
 import Layout from '@components/Layout'
 import { API_URL } from '@config/index'
+import styles from '@styles/Event.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 export default function SingleEventPage({ event }) {
+	const deleteEvent = e => {
+		console.log('delete')
+	}
 	return (
 		<Layout>
-			.
+			<div className={styles.event}>
+				<div className={styles.controls}>
+					<Link href={`/events/edit/${event.id}`}>
+						<a>
+							<FaPencilAlt /> Edit Event
+						</a>
+					</Link>
+					<a href="#" className={styles.delete} onClick={deleteEvent}>
+						<FaTimes /> Delete Event
+					</a>
+				</div>
+				<span>
+					{event.date} at {event.time}
+				</span>
+				<h1>{event.name}</h1>
+				{event.image && (
+					<div className={styles.image}>
+						<Image
+							src={event.image}
+							alt={event.slug}
+							width={960}
+							height={600}
+						/>
+					</div>
+				)}
+				<h3>Performers: </h3>
+				<p>{event.performers}</p>
+				<h3>Description: </h3>
+				<p>{event.description}</p>
+				<h3>Venue: {event.venue}</h3>
+				<p>{event.address}</p>
+
+				<Link href="/events">
+					<a className={styles.back}>{'<'} Go Back</a>
+				</Link>
+			</div>
 		</Layout>
 	)
 }
